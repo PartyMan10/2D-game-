5a77626f2cde1581ba061a16a174c6abdbfa1025
packman <bidhiadhikari29@gmail.com>
2025-12-04 08:08:10 -0500

    ok
    just 4 i guess

--- 2-d-game-/2-d-game-/ASSET/SCRIPT/player-controler.gd
+++ 2-d-game-/2-d-game-/ASSET/SCRIPT/player-controler.gd
@@ -12,8 +12,15 @@ var direction = 0
 var jump_multiplier = -30.0
 # const SPEED = 300.0
 # const JUMP_VELOCITY = -500.0
-
-
+func _input(event):
+	# Handle jump.
+	if event.is_action_pressed("jump")and is_on_floor():
+		velocity.y = Jump_power * jump_multiplier
+	#handle jump down 
+	if event.is_action_pressed("move_down "):
+		set_collision_mask_value(10,false)
+	else:
+		set_collision_mask_value(10,true)
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
