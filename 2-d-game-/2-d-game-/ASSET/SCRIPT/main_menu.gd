08ba46ff33a2c16bf85c18f0b04daeaedd34eabf
Lyra <lyraholland2010@gmail.com>
2025-12-04 09:43:50 -0500

    init
    allalalalalal

--- 2-d-game-/2-d-game-/ASSET/SCRIPT/main_menu.gd
+++ 2-d-game-/2-d-game-/ASSET/SCRIPT/main_menu.gd
@@ -7,7 +7,7 @@ func _ready():
 
 func _on_Play_pressed():
 	# Replace with your actual game scene path
-	get_tree().change_scene_to_file("res://Game.tscn")
+	get_tree().change_scene_to_file("res://ASSET/SCENE/AREA/area_1.tscn")
 
 func _on_Quit_pressed():
 	get_tree().quit()
