d73ca820a88c0e0696ce4fbd69391af35f738ed2
Lyra <lyraholland2010@gmail.com>
2025-12-04 13:08:47 -0500

    init
    gravity works

--- 2-d-game-/2-d-game-/ASSET/SCRIPT/player-controler.gd
+++ 2-d-game-/2-d-game-/ASSET/SCRIPT/player-controler.gd
@@ -1,16 +1,28 @@
 extends CharacterBody2D
 
 const SPEED = 200
-const GRAVITY = 800
+const GRAVITY = 1200
 const JUMP_FORCE = -400
 
 func _physics_process(delta):
-	velocity.y += GRAVITY * delta
+	# Apply gravity
+	if not is_on_floor():
+		velocity.y += GRAVITY * delta
 
-	var direction = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	# Horizontal movement
+	var direction = Input.get_axis("ui_left", "ui_right")
 	velocity.x = direction * SPEED
 
+	# Jump
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_FORCE
 
+	# Move with collisions
 	move_and_slide()
+
+	# Animations
+	if direction != 0:
+		$AnimatedSprite2D.play("walk")
+		$AnimatedSprite2D.flip_h = direction < 0
+	else:
+		$AnimatedSprite2D.play("idle")
